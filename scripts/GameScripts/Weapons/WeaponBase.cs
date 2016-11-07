1027793e51e4a49a012fbcc40c0505c744ff2b5e
Devereaux Legakis <devereaux98@verizon.net>
2016-11-07 11:49:53 -0700

    Commit

--- scripts/GameScripts/Weapons/WeaponBase.cs
+++ scripts/GameScripts/Weapons/WeaponBase.cs
@@ -3,9 +3,25 @@ public class WeaponBase
 {
     public int power = 10;
     public float range = 1.0f;
+    public int ammoCount = 100;
 
     public void fire()
     {
-        Console.Write("Fire");
+        for (int i = ammoCount; i > 0; i--)
+        {
+            ammoCount -- ;
+            Console.Write("Fire " + ammoCount);
+        }
+    }
+
+    public void SemiAutoFire()
+    {
+        int i = ammoCount;
+        while (i > 0)
+        {
+            ammoCount -- ;
+            Game.GameTimer();
+            Console.Write("Fire " + ammoCount);
+        }
     }
 }
